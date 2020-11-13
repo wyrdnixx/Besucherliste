dd5dfdfc83c8ebba16fb5477f2138ba331c738ab
wyrdnixx <jojo@ulewu.de>
2020-11-13 01:07:18 +0100

    refactor "hub" -> Multi clients

--- main.go
+++ main.go
@@ -1,16 +1,51 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"flag"
+	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
 	_ "github.com/wyrdnixx/Besucherliste/models"
 	"github.com/wyrdnixx/Besucherliste/modules"
 )
 
-var upgrader = websocket.Upgrader{
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "websockets.html")
+}
+
+func main() {
+	flag.Parse()
+	hub := modules.NewHub()
+	go hub.Run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		modules.ServeWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+/*
+var upgrade
+
+r = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
@@ -50,3 +85,4 @@ func main() {
 
 	http.ListenAndServe(":8080", nil)
 }
+*/
