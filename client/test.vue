4d1e72e426c734f3388fd1c9473b5715bd39f47f
wyrdnixx <jojo@ulewu.de>
2021-01-01 21:01:52 +0100

    test with vue - nothing

---
+++ client/test.vue
@@ -0,0 +1,6 @@
+<template>
+oksdfkos
+    <div id="xxxxx" style="background-color:rgb(40, 40, 40); padding-left: 3%; padding-right: 3%;" >
+        Hallo
+    </div>
+</template>
--- main.go
+++ main.go
@@ -17,15 +17,21 @@ var addr = flag.String("addr", ":8080", "http service address")
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "websockets.html")
+
+	/*
+		if r.URL.Path != "/" {
+			http.Error(w, "Not found", http.StatusNotFound)
+			return
+		}
+		if r.Method != "GET" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		//http.ServeFile(w, r, "websockets.html")
+
+	*/
+	w.Header().Set("contend-type", "application/javasript")
+	http.ServeFile(w, r, "client/index.html")
 }
 
 func main() {
@@ -34,7 +40,12 @@ func main() {
 	hub := modules.NewHub()
 	go hub.Run()
 	http.HandleFunc("/", serveHome)
+
+	//fs := http.FileServer(http.Dir("client"))
+	//http.Handle("/", fs)
+
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+
 		modules.ServeWs(hub, w, r)
 	})
 	err := http.ListenAndServe(*addr, nil)
@@ -42,48 +53,3 @@ func main() {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
-
-/*
-var upgrade
-
-r = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-// Test kommentar
-func main() {
-
-	modules.ReadConfig()
-
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
-
-		for {
-			// Read message from browser
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return
-			}
-
-			// Print the message to the console
-			fmt.Printf("%s sent msg: %s\n", conn.RemoteAddr(), string(msg))
-			var res = modules.ConsumeMessage(msg)
-
-			// write result Message to the Client
-			b, err := json.Marshal(res)
-			if err = conn.WriteMessage(msgType, b); err != nil {
-				fmt.Printf("Error sending result: %s", err.Error())
-				return
-			}
-
-		}
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "websockets.html")
-	})
-
-	http.ListenAndServe(":8080", nil)
-}
-*/
